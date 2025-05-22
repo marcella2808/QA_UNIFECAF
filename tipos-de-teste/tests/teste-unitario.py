e5f9b9059fdc907dbe5ee0ee434723c8d48cfa9c
Marcella <135839101+marcella2808@users.noreply.github.com>
2025-05-22 09:08:57 -0300

    Create teste-unitario.py

---
+++ tipos-de-teste/tests/teste-unitario.py
@@ -0,0 +1,10 @@
+from tipos-de-teste.usuario-service import cadastrar_usuario.py
+
+def test_cadastro_usuario():
+    assert cadastrar_usuario("João", "18374829387", "joao@email.com", "1234") == "Usuário cadastrado com sucesso!"
+    assert cadastrar_usuario("Maria", "8374", "maria@email.com", "abcdefgh") == "CPF deve conter 11 dígitos."
+    assert cadastrar_usuario("Caroline", "47283748293", "caroline@email.com", "123") == "A senha deve ter pelo menos 4 dígitos."
+    assert len(usuarios) == 1
+    print("Teste unitário de cadastro de usuário passou!")
+
+test_cadastro_usuario()
