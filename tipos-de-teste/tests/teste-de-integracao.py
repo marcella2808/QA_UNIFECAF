b34d3bb1529ef7aa76bc0b380f051260b4f5f153
Marcella <135839101+marcella2808@users.noreply.github.com>
2025-05-22 09:12:22 -0300

    Update teste-de-integracao.py

--- tipos-de-teste/tests/teste-de-integracao.py
+++ tipos-de-teste/tests/teste-de-integracao.py
@@ -1,8 +1,9 @@
-from tipos-de-teste.usuario-service import login_usuario.py
+from tipos-de-teste.usuario-service import login_usuario
 
 def test_integracao_login():
     assert login_usuario("joao@email.com", "1234") == "Login bem-sucedido"
     assert login_usuario("joao@email.com", "senhaerrada") == "Falha no login"
+    assert login_usuario("", "1234") == "Preencha todos os campos"
     print("Teste de integração entre cadastro e login passou!")
 
 test_integracao_login()
