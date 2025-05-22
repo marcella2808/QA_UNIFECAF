c72905896391d49ce705b14c4c4858a9baf2f3c1
Marcella <135839101+marcella2808@users.noreply.github.com>
2025-05-22 09:12:42 -0300

    Update teste-de-sistema.py

--- tipos-de-teste/tests/teste-de-sistema.py
+++ tipos-de-teste/tests/teste-de-sistema.py
@@ -4,6 +4,8 @@ def test_sistema_carrinho():
     carrinho = CarrinhoDeCompras()
     carrinho.adicionar_item("Camiseta")
     carrinho.adicionar_item("Tênis")
+    carrinho.adicionar_item("Short")
+    carrinho.excluir_item("Tênis")
     assert len(carrinho.itens) == 2
     assert carrinho.finalizar_compra() == "Compra finalizada com sucesso!"
     print("Teste de sistema do carrinho de compras passou!")
