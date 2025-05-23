e6a487ffccf842980ac5133350eb4cbd22da762b
Marcella <135839101+marcella2808@users.noreply.github.com>
2025-05-23 08:14:26 -0300

    Update teste-de-regressao.py

--- tipos-de-teste/tests/teste-de-regressao.py
+++ tipos-de-teste/tests/teste-de-regressao.py
@@ -1,5 +1,3 @@
-from tipos-de-teste.usuario-service import login_usuario
-
 def test_regressao_login():
     assert login_usuario("joao@email.com", "1234") == "Login bem-sucedido"
     print("Teste de regressão para o login passou!")
